4d0b2e8148a2ca44f7eace72cdd5648a3737332a
dhanushcodie <dhanushdotdev@gmail.com>
2024-07-17 01:44:22 +0530

    removing auto-mx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
             {/* <!-- Mobile menu button--> */}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-    <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-10 sm:mt-20 md:mt-30">
+    <div className="max-w-7xl px-6 lg:px-8 mt-10 sm:mt-20 md:mt-30">
       <h1 className="font-display text-2xl font-semibold text-neutral-50">
         Contact Us
       </h1>
